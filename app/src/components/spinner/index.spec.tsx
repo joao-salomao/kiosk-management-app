df89d197fc83a87d6cbcefc971cbed824de06978
João Salomão <joaosalomao.613@gmail.com>
2022-06-25 01:53:29 -0300

    test: spinner

---
+++ app/src/components/spinner/index.spec.tsx
@@ -0,0 +1,12 @@
+import renderer from 'react-test-renderer';
+import { Spinner } from '.'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Spinner />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
