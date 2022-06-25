a642e4267691c217809b8e4608ce0060a931d812
João Salomão <joaosalomao.613@gmail.com>
2022-06-25 03:18:05 -0300

    test: snap when is not submitting the form

--- app/src/components/pages/kiosk/partials/form/index.spec.tsx
+++ app/src/components/pages/kiosk/partials/form/index.spec.tsx
@@ -2,39 +2,6 @@ import renderer from 'react-test-renderer';
 import { fireEvent, screen, render, waitFor } from '@testing-library/react';
 import { Form } from ".";
 
-
-describe('when is submitting the form', () => {
-  it('disable the submit button', () => {
-    render(<Form isSubmitting={true} onSubmit={jest.fn()} />)
-
-    const button = screen.getByRole('button');
-
-    expect(button).toBeDisabled();
-  });
-
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(
-        <Form isSubmitting={true} onSubmit={jest.fn()} />
-      )
-      .toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
-})
-
-it('show the required validation text when the fields values are invalid', async () => {
-  render(<Form isSubmitting={false} onSubmit={jest.fn()} />)
-
-  const button = screen.getByRole('button');
-  fireEvent.click(button);
-
-  await waitFor(() => {
-    const elements = screen.queryAllByText(/this field is required/i);
-    expect(elements).toHaveLength(3);
-  });
-});
-
 it('call the onSubmit handler when all fields are valid', async () => {
   const onSubmitMock = jest.fn();
 
@@ -64,5 +31,49 @@ it('call the onSubmit handler when all fields are valid', async () => {
 
   await waitFor(() => {
     expect(onSubmitMock).toHaveBeenCalled();
+  });
+});
+
+describe('when is submitting the form', () => {
+  it('disable the submit button', () => {
+    render(<Form isSubmitting={true} onSubmit={jest.fn()} />)
+
+    const button = screen.getByRole('button');
+
+    expect(button).toBeDisabled();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Form isSubmitting={true} onSubmit={jest.fn()} />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('when is not submitting the form', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Form isSubmitting={false} onSubmit={jest.fn()} />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+it('show the required validation text when the fields values are invalid', async () => {
+  render(<Form isSubmitting={false} onSubmit={jest.fn()} />)
+
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
+
+  await waitFor(() => {
+    const elements = screen.queryAllByText(/this field is required/i);
+    expect(elements).toHaveLength(3);
   });
 });
