ac46ccda9f5876884c18cc0471744fd2c42c8156
João Salomão <joaosalomao.613@gmail.com>
2022-06-25 06:16:15 -0300

    fix: env vars

--- api/src/database/data-source.ts
+++ api/src/database/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || "test",
+  password: process.env.DB_PASSWORD || "test",
+  database: process.env.DB_DATABASE || "test",
   synchronize: false,
   logging: true,
   entities: ["src/database/entities/*.ts"],
