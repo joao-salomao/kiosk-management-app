fee926e886f8798352fb283f805a2627770b1222
João Salomão <joaosalomao.613@gmail.com>
2022-06-25 05:15:25 -0300

    feat: setup dotenv

--- api/src/database/data-source.ts
+++ api/src/database/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "test",
-  password: "test",
-  database: "test",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: false,
   logging: true,
   entities: ["src/database/entities/*.ts"],
